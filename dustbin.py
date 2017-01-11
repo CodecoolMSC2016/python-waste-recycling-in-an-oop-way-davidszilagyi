a3619b27987e59c4bf1807fb1f50a813d879564e
David Szilagyi <szilagyidav@gmail.com>
2017-01-11 18:17:23 +0100

    All test run well :)

--- dustbin.py
+++ dustbin.py
@@ -5,4 +5,29 @@ from dustbin_content_error import DustbinContentError
 
 
 class Dustbin:
-    pass
+    def __init__(self, color):
+        self.color = color
+        self.paper_content = []
+        self.plastic_content = []
+        self.house_waste_content = []
+
+    def throw_out_garbage(self, garbage):
+        if isinstance(garbage, PaperGarbage):
+            if garbage.is_squeezed is True:
+                self.paper_content.append(garbage)
+            else:
+                raise DustbinContentError("The garbage is not clean!")
+        elif isinstance(garbage, PlasticGarbage):
+            if garbage.is_clean is True:
+                self.plastic_content.append(garbage)
+            else:
+                raise DustbinContentError("The garbage is not squeezed!")
+        elif isinstance(garbage, Garbage):
+            self.house_waste_content.append(garbage)
+        else:
+            raise DustbinContentError("This not a garbage!")
+
+    def empty_contents(self):
+        self.paper_content = []
+        self.plastic_content = []
+        self.house_waste_content = []
